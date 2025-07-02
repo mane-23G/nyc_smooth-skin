7ae8854dffc6b145c4bf1a8de71b366dc31f6ed3
mane-23G <bobfisherman30@gmail.com>
2025-07-01 23:04:48 -0400

    laser page and faq updated

--- nyc-smooth-skin/src/pages/FAQ.tsx
+++ nyc-smooth-skin/src/pages/FAQ.tsx
@@ -3,7 +3,73 @@ import { PageName } from "../components/PageName";
 export function FAQ() {
     return(
         <div>
-            <PageName title="FAQ"/>
+            <div>
+                <PageName title="FAQ"/>
+            </div>
+            <div className="faq">
+                <p className="question">
+                    How does Elite+ technology works to remove the facial discoloration?
+                </p>
+                <p className="answer">
+                    The special wavelength selectively targets only blood in the tiny veins without affecting superficial skin or surrounding tissues making this the “gold standard” and safest treatment out there. The laser  uses selective light which likes blood inside your tiny veins. Once the light gets absorbed by the blood in the vein, it gets heated up causing the veins to shrink and eventually fade, leaving behind beautiful skin which you can show off!! No need to cover up with heavy makeup.
+                    <br></br>
+                    A gel will be applied to the areas of treatment.  This gel acts as a safety barrier between the laser and your skin. You will be given protective goggles to wear and you will feel as the laser hand piece is moving up and down your face or the nose.
+                </p>
+                <p className="question">
+                    How does laser  procedure feel?
+                </p>
+                <p className="answer">
+                    It feels like mild warming sensation on the skin. It is generally very comfortable treatment and  specialair cooling to help with any discomfort if it develops.
+                </p>
+                <p className="question">
+                    How long does the treatment take?   
+                </p>
+                <p className="answer">
+                    It depends on the area to be treated and the number of veins to be treated. On average it takes between 5- 25 minutes per treatment.
+                </p>
+                <p className="question">
+                   Is there any downtime after my Elite + treatment?
+                </p>
+                <p className="answer">
+                    There might be transient redness of the face for about 30 min to 2 hours. Nothing really crazy.
+                </p>
+                <p className="question">
+                    Can I apply make up after my Elite + treatment?
+                </p>
+                <p className="answer">
+                    Yes, it's is safe to apply makeup as no damage to the skin surface was made.
+                </p>
+                <p className="question">
+                    When will I start noticing results after my treatment?
+                </p>
+                <p className="answer">
+                    It depends.  Some vessels will disappear immediately during the treatment, while others may turn slightly grey, or purple.  You might notice slight improvement in blushing, but it may take more than one treatment.  The response varies from person to person. 
+                </p>
+                <p className="question">
+                    How many treatments will I need?
+                </p>
+                <p className="answer">
+                    This answer depends on the size, location and the severity of your veins.  This will be discussed in detail during your consultation.  On Average 3-5 treatments are needed to achieve the desired aesthetic result. Results and duration of effect may vary, no guarantee of specific results can be given. 
+                </p>
+                <p className="question">
+                    How long should I wait between my treatments?
+                </p>
+                <p className="answer">
+                    It is recommended to wait 2-4 weeks between treatment.
+                    <br></br>
+                    The price depends on the area to be treated as some patients have rosacea on their cheeks, while others have it all over their face. Prices generally start at $150 per area as this is a very effective very, quick, and affordable treatment that is worth doing to eliminate skin discoloration.
+                </p>
+                <p className="question">
+                    What does laser genesis do for your face?
+                </p>
+                <p className="answer">
+                    Laser Genesis is a non-invasive facial treatment that utilizes laser technology to gently heat the skin, stimulating collagen production and improving various skin conditions. It's known for reducing the appearance of fine lines and wrinkles, diminishing redness and rosacea,mimproving the appearance of acne scars and discoloration, minimizeing pore size, and reversing sun damage.
+                    <br></br>
+
+                </p>
+                <br></br>
+                <p style={{textAlign:"center", fontSize:"20px"}}>If you have any questions that are still unanswered, please feel free to <a href="/ContactUs">Contact Us</a>!</p>
+            </div>
         </div>
         
     )
